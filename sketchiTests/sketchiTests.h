b8ec08ba214e3ae58edadfab131578e5b609d6c0
Nicolas Robertson <nrobertson@oucs1384.otago.ac.nz>
2012-05-15 13:14:33 +1200

    Initial Commit

---
+++ sketchiTests/sketchiTests.h
@@ -0,0 +1,13 @@
+//
+//  sketchiTests.h
+//  sketchiTests
+//
+//  Created by Nicolas Robertson on 5/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface sketchiTests : SenTestCase
+
+@end
---
+++ sketchiTests/sketchiTests.m
@@ -0,0 +1,32 @@
+//
+//  sketchiTests.m
+//  sketchiTests
+//
+//  Created by Nicolas Robertson on 5/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "sketchiTests.h"
+
+@implementation sketchiTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in sketchiTests");
+}
+
+@end
