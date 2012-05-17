f3d4bd218cf8f375677f51beee65b25bddd4e9ff
Nic Robertson <Nicrob64@gmail.com>
2012-05-17 17:22:15 +1200

    Added menus and menu functionalities

--- sketchi/sketchiViewController.h
+++ sketchi/sketchiViewController.h
@@ -10,14 +10,33 @@
 #import <QuartzCore/QuartzCore.h>
 
 @interface sketchiViewController : UIViewController {
-    UIImageView *drawImage, *menuScreen;
-    IBOutlet UIButton *menu, *saveImage, *clear, *back, *brushOptionButton, *brush1, *brush2, *brush3 ;
+    UIImageView *drawImage;
+    IBOutlet UIViewController *brushOptionMenu, *mainMenu;
+    IBOutlet UIButton *menu, *saveImage, *clear, *back, *brushOptionButton, *brush1, *brush2, *brush3, *cancelBrushMenu, *backToDrawingBrushMenu;
+    IBOutlet UISlider *red, *green, *blue, *sizeSlider;
+    IBOutlet UILabel *colourLabel;
+    IBOutlet UISwitch *cyclicSwitch;
     int mouseMoved, brushOption;
     BOOL mouseSwiped, rmax, gmax, bmax, bmin, gmin, rmin, cyclic, tiltDraw;
     CGPoint lastPoint;
-    double b;
-    double r;
-    double g;
+    double b, r, g, brushSize;
 }
-@property (nonatomic,retain) UIButton *menu, *saveImage, *clear, *back, *brushOptionButton, *brush1, *brush2, *brush3 ;
+@property (nonatomic,retain) UIButton *menu, *saveImage, *clear, *back, *brushOptionButton, *brush1, *brush2, *brush3, *cancelBrushMenu, *backToDrawingBrushMenu;
+@property (nonatomic,retain) UIViewController *brushOptionMenu, *mainMenu;
+@property (nonatomic,retain) UISlider *red, *green, *blue, *sizeSlider;
+@property (nonatomic,retain) UILabel *colourLabel;
+@property (nonatomic,retain) UISwitch *cyclicSwitch;
+
+-(IBAction) brushOptionClick:(id)sender;
+-(IBAction) menuButtonClick:(id)sender;
+-(IBAction) saveButtonClick:(id)sender;
+-(IBAction) clearButtonClick:(id)sender;
+-(IBAction) backButtonClick:(id)sender;
+-(IBAction) cancelBrushMenuButtonClick:(id)sender;
+-(IBAction) cyclicSwitchClick:(id)sender;
+-(IBAction) colourSlider:(id)sender;
+-(IBAction) backToDrawingBrushMenu:(id)sender;
+-(IBAction) brushType0:(id)sender;
+-(IBAction) brushType1:(id)sender;
+-(IBAction) brushType2:(id)sender;
 @end
--- sketchi/sketchiViewController.m
+++ sketchi/sketchiViewController.m
@@ -18,19 +18,23 @@
 @synthesize brush1;
 @synthesize brush2;
 @synthesize brush3;
+@synthesize brushOptionMenu;
+@synthesize mainMenu;
+@synthesize cancelBrushMenu;
+@synthesize backToDrawingBrushMenu;
+@synthesize red, green, blue, sizeSlider;
+@synthesize colourLabel;
+@synthesize cyclicSwitch;
 
 - (void) viewDidLoad {
     [super viewDidLoad];
-    [saveImage addTarget:self action:@selector(saveButtonClick:) forControlEvents:(UIControlEvents)UIControlEventTouchDown];
-    [clear addTarget:self action:@selector(clearButtonClick:) forControlEvents:(UIControlEvents)UIControlEventTouchDown];
-    [menu addTarget:self action:@selector(menuButtonClick:) forControlEvents:(UIControlEvents)UIControlEventTouchDown];
-    [back addTarget:self action:@selector(backButtonClick:) forControlEvents:(UIControlEvents)UIControlEventTouchDown];
-    [brushOptionButton addTarget:self action:@selector(brushOptionClick) forControlEvents:(UIControlEvents)UIControlEventTouchDown];
     drawImage = [[UIImageView alloc] initWithImage:nil];
     drawImage.frame = self.view.frame;
+    //drawImage.frame = CGRectMake(0,0,self.view.frame.size.width, self.view.frame.size.height-30); //very interesting bug, check it out
     [self.view addSubview:drawImage];
     mouseMoved = 0;
-    r = 1.0;
+    brushSize = 10.0;
+    r = 0.0;
     b = 0.0;
     g = 0.0;
     rmax = true;
@@ -39,10 +43,8 @@
     rmin = false;
     bmin = true;
     gmin = true;
-    cyclic = true;
-    back.hidden = 1;
+    cyclic = false;
     brushOption = 0;
-    brushOptionButton.hidden = 1;
 }
 
 -(void)touchesBegan:(NSSet *) touches withEvent:(UIEvent *)event {
@@ -53,38 +55,79 @@
 	lastPoint.y -= 20;
 }
 
--(void) brushOptionClick:(id)sender{
-    brush1.hidden = 0;
+-(IBAction)brushType0:(id)sender{
+    brushOption = 0;
+}
+
+-(IBAction)brushType1:(id)sender{
+    brushOption = 1;
+}
+
+-(IBAction)brushType2:(id)sender{
+    brushOption = 2;
+}
+
+-(IBAction)backToDrawingBrushMenu:(id)sender{
+    [brushOptionMenu.view removeFromSuperview];
+    drawImage.hidden = 0;
+    menu.hidden = 0;
+    saveImage.hidden = 0;
+    clear.hidden = 0;
+    [mainMenu.view removeFromSuperview];
+}
+
+-(IBAction)sizeSlider:(id)sender{
+    brushSize = sizeSlider.value;
+}
+
+-(IBAction)colourSlider:(id)sender{
+    [colourLabel setTextColor:[UIColor colorWithRed:red.value green:green.value blue:blue.value alpha:1.0]];
+    r = red.value;
+    b = blue.value;
+    g = green.value;
+}
+
+-(IBAction)cyclicSwitchClick:(id)sender{
+    r = 1.0;
+    b = 0.0;
+    g = 0.0;
+    cyclic = cyclicSwitch.isEnabled;
+}
+
+-(IBAction)cancelBrushMenuButtonClick:(id)sender{
+    [brushOptionMenu.view removeFromSuperview];
+}
+
+-(IBAction) brushOptionClick:(id)sender{
+    [self.view addSubview:brushOptionMenu.view];
     
     
 }
--(void) backButtonClick:(id)sender{
+-(IBAction) backButtonClick:(id)sender{
     
     drawImage.hidden = 0;
     menu.hidden = 0;
     saveImage.hidden = 0;
     clear.hidden = 0;
-    back.hidden = 1;
-    brushOptionButton.hidden = 1;
+    [mainMenu.view removeFromSuperview];
     
 }
 
--(void) menuButtonClick:(id)sender{
-    
+-(IBAction) menuButtonClick:(id)sender{
     drawImage.hidden = 1;
     menu.hidden = 1;
     saveImage.hidden = 1;
     clear.hidden = 1;
-    back.hidden = 0;
-    brushOptionButton.hidden = 0;
+    [self.view addSubview:mainMenu.view];
+   
 }
 
--(void) clearButtonClick:(id)sender {
+-(IBAction) clearButtonClick:(id)sender {
     drawImage.image = nil;
     return;
 }
 
--(void) saveButtonClick:(id)sender {
+-(IBAction) saveButtonClick:(id)sender {
 
         UIGraphicsBeginImageContext(CGSizeMake(320,480));
         CGContextRef context = UIGraphicsGetCurrentContext();
@@ -101,7 +144,7 @@
     if(!drawImage.hidden){
     if(cyclic){
         if(bmax && !rmax && gmin){
-            r +=0.01;
+            r +=0.02;
             rmin = false;
             if(r >= 1.0){
                 rmax = true;
@@ -109,35 +152,35 @@
         }
         
         if(!bmin && rmax && gmin){
-            b -=0.01;
+            b -=0.02;
             bmax = false;
             if(b <= 0.0){
                 bmin = true;
             }
         }
         if(bmin && rmax && !gmax){
-            g +=0.01;
+            g +=0.02;
             gmin = false;
             if(g >= 1.0){
                 gmax = true;
             }
         }
         if(bmin && !rmin && gmax){
-            r -=0.01;
+            r -=0.02;
             rmax = false;
             if(r <= 0.0){
                 rmin = true;
             }
         }
         if(!bmax && rmin && gmax){
-            b += 0.01;
+            b += 0.02;
             bmin = false;
             if(b >= 1.0){
                 bmax = true;
             }
         }
         if(bmax && rmin && !gmin){
-            g -=0.01;
+            g -=0.02;
             gmax = false;
             if(g <= 0.0){
                 gmin = true;
@@ -150,8 +193,16 @@
     currentPoint.y -= 20; // only for 'kCGLineCapRound'
     UIGraphicsBeginImageContext(self.view.frame.size);
     [drawImage.image drawInRect:CGRectMake(0, 0, drawImage.frame.size.width, drawImage.frame.size.height)]; //originally self.frame.size.width, self.frame.size.height)];
+        if(brushOption == 0){
     CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapRound); //kCGLineCapSquare, kCGLineCapButt, kCGLineCapRound
-    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), 10.0); // for size
+        }
+        if(brushOption == 1){
+            CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapSquare); //kCGLineCapSquare, kCGLineCapButt, kCGLineCapRound
+        }
+        if(brushOption == 2){
+            CGContextSetLineCap(UIGraphicsGetCurrentContext(), kCGLineCapButt); //kCGLineCapSquare, kCGLineCapButt, kCGLineCapRound
+        }
+    CGContextSetLineWidth(UIGraphicsGetCurrentContext(), brushSize); // for size
     CGContextSetRGBStrokeColor(UIGraphicsGetCurrentContext(), r, g, b, 1.0); //values for R, G, B, and Alpha
     CGContextBeginPath(UIGraphicsGetCurrentContext());
     CGContextMoveToPoint(UIGraphicsGetCurrentContext(), lastPoint.x, lastPoint.y);
@@ -167,6 +218,9 @@
     if (mouseMoved == 10) {
         mouseMoved = 0;
     }
+        red.value = r;
+        green.value = g;
+        blue.value =b;
     }
 }
    /* 
